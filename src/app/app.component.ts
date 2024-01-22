e90cb1c0acafc310236426d2c8536018464a736e
Asset Kad <asset_kad@mail.ru>
2024-01-22 07:03:48 +0500

    first and second task finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,79 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { WalletService } from './services/wallet.service';
+import { Observable, Subject, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'oneinchcrypto';
+  title = '1inch crypto';
 
-  public walletID: number | undefined;
+  public walletIDs$ = new Subject<String[]>();
 
-  constructor(public walletService: WalletService) {
-    this.checkWalletConnected();
-  }
+  public currentAccount = 0;
 
-  connectToWallet = () => {
-    this.walletService.connectWallet();
-  };
+  public isConnecting = false;
 
-  checkWalletConnected = async () => {
-    const accounts = await this.walletService.checkWalletConnected();
-    if (accounts.length > 0) {
-      this.walletID = accounts[0];
-    }
-  };
+  constructor(public walletService: WalletService) {}
 
   ngOnInit(): void {
     this.checkWalletConnected();
+    this.loadCurrentAccount();
   }
+
+  private saveCurrentAccount() {
+    localStorage.setItem('currentAccount', `${this.currentAccount}`);
+  }
+
+  private loadCurrentAccount() {
+    const account = localStorage.getItem('currentAccount');
+    if (account !== null && account !== undefined) {
+      this.currentAccount = +account;
+    }
+  }
+
+  onAccountChange() {
+    this.saveCurrentAccount();
+    this.walletIDs$.pipe(take(1)).subscribe((walletIDs) => {
+      console.log(walletIDs);
+      this.walletService.getEthBalance(walletIDs[this.currentAccount]);
+      this.walletService.getWethBalance(walletIDs[this.currentAccount]);
+    });
+  }
+
+  connectToWallet = () => {
+    this.isConnecting = true;
+    this.walletService.connectWallet().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.walletIDs$.next(res);
+        this.isConnecting = false;
+      },
+      error: (error) => {
+        console.error(error);
+        this.isConnecting = false;
+      },
+      complete: () => {
+        this.isConnecting = false;
+        this.checkWalletConnected();
+      },
+    });
+  };
+
+  checkWalletConnected = async () => {
+    try {
+      const accounts = await this.walletService.checkWalletConnected();
+      if (accounts.length > 0) {
+        console.log(accounts);
+        this.walletIDs$.next(accounts);
+        this.walletService.getEthBalance(accounts[this.currentAccount]);
+        this.walletService.getWethBalance(accounts[this.currentAccount]);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
 }
--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -1,36 +1,105 @@
+import {
+  EMPTY,
+  Observable,
+  catchError,
+  from,
+  of,
+  switchMap,
+  throwError,
+} from 'rxjs';
 import { Injectable } from '@angular/core';
+import { ethers, utils } from 'ethers';
+import { abi } from '../core/contract/contract-abi';
+import { Web3Provider } from '@ethersproject/providers';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WalletService {
   public ethereum;
+  contractProps: any = {};
+  public currentAccount = 0;
+  provider!: Web3Provider;
 
   constructor() {
-    const { ethereum } = <any>window;
+    const { ethereum } = window;
     this.ethereum = ethereum;
+    if (!ethereum) {
+      console.error('Metamask not found. Please install Metamask.');
+    }
+    this.provider = new ethers.providers.Web3Provider(ethereum);
   }
 
-  public connectWallet = async () => {
-    try {
-      if (!this.ethereum) return alert('Please install metamask');
-      const res = await this.ethereum.request({
-        method: 'eth_requestAccounts',
-      });
-      console.log(res);
-    } catch (e) {
-      throw new Error('Not found ethereum');
+  async getEthBalance(address: any): Promise<any> {
+    if (!this.provider) {
+      throw new Error('Wallet not connected.');
     }
+    const balance = await this.provider.getBalance(address);
+    console.log({ ethBalance: utils.formatEther(balance) });
+    return { ethBalance: utils.formatEther(balance) };
+  }
+
+  async getWethBalance(address: any): Promise<any> {
+    if (!this.provider) {
+      throw new Error('Wallet not connected.');
+    }
+    const contract = new ethers.Contract(
+      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      abi,
+      this.provider
+    );
+    const balance = await contract['balanceOf'](address);
+    const res = { wethBalance: ethers.utils.formatUnits(balance, 18) }; // убрать
+    console.log(res);
+    return res;
+  }
+
+  public connectWallet = (): Observable<any> => {
+    const requestAccounts$ = from(
+      this.ethereum?.request({ method: 'eth_requestAccounts' })
+    );
+
+    return requestAccounts$.pipe(
+      switchMap((accounts: any) => {
+        return of(accounts);
+      }),
+      catchError((error: any) => this.handleError(error))
+    );
   };
+
+  private handleError(error: any): Observable<any> {
+    console.error(error);
+
+    if (
+      error.message === 'Already processing eth_requestAccounts. Please wait.'
+    ) {
+      const requestPermissions$ = from(
+        window.ethereum.request({
+          method: 'wallet_requestPermissions',
+          params: [{ eth_accounts: {} }],
+        })
+      );
+
+      return requestPermissions$.pipe(
+        switchMap(() => this.connectWallet()),
+        catchError(() => of(undefined))
+      );
+    } else {
+      return throwError(() => error);
+    }
+  }
 
   public checkWalletConnected = async () => {
     try {
-      if (!this.ethereum) return alert('Please install meta mask ');
+      if (!this.ethereum) {
+        console.error('Metamask not found. Please install Metamask.');
+        return [];
+      }
+
       const accounts = await this.ethereum.request({ method: 'eth_accounts' });
-      console.log(accounts);
       return accounts;
     } catch (e) {
-      throw new Error('No ethereum object found');
+      throw new Error('Failed to check wallet connection.');
     }
   };
 }
